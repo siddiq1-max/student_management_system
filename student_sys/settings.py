f710b5a4b1e83807a950ffdd124144030516ade1
siddiq1-max <siddiqsaliyath@gmail.com>
2026-01-15 14:44:19 +0530

    Fix Vercel deployment: Add whitenoise, configure static root, alias wsgi app

--- student_sys/settings.py
+++ student_sys/settings.py
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
